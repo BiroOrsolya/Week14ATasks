5753ebf38964d0a465eeb0b4588fdeacc4a897a4
biroorsolya <birorso@gmail.com>
2016-04-05 20:46:41 +0200

    Dictionaries

---
+++ DictionaryCollections/DictionaryCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace DictionaryCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht["0"] = "zero";
+            ht["1"] = "one";
+            ht["2"] = "two";
+            ht["3"] = "three";
+            ht["4"] = "four";
+            ht["5"] = "five";
+            ht["6"] = "six";
+            ht["7"] = "seven";
+            ht["8"] = "eight";
+            ht["9"] = "nine";
+
+            string str = "67469246";
+
+            foreach (char c in str)
+            {
+                string digit = c.ToString();
+                if (ht.ContainsKey(digit))
+                {
+                    Console.WriteLine(ht[digit]);
+                }
+            }
+        }
+    }
+}
---
+++ GenericCollections/GenericCollections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, String> dict = new Dictionary<int, String>();
+
+            dict[36] = "Hungary";
+            dict[666] = "PonyLand";
+            dict[0] = "Nowhere";
+            dict[13] = "United Kingdom";
+
+            Console.WriteLine("The 666 code is for: {0}", dict[666]);
+
+            foreach (KeyValuePair<int, String> item in dict)
+            {
+                int code = item.Key;
+                string country = item.Value;
+                Console.WriteLine("Code {0} = {1}", code, country);
+            }
+            Console.Read();
+        }
+    }
+}
